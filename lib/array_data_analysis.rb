9f8710e1259ab9cacc2e763475b9396e7e4a2a94
roryg11 <Rory.C.Grant@gmail.com>
2014-11-07 08:31:11 -0800

    array data analysis file

--- lib/array_data_analysis.rb
+++ lib/array_data_analysis.rb
@@ -1,2 +1,54 @@
 class ArrayDataAnalysis
+  # call on the TEMPERATURE_DATA
+  # make a variable for each column of data
+  # make methods to evaluate each of those
+  def initialize(data)
+    @data = data
+    @lows = lows
+    @highs = highs
+  end
+
+  def lows
+    @lows
+  end
+
+  def highs
+    @highs
+  end
+
+  def lowest_temperature
+    # @data[8][2]
+    lows = []
+    @data.each do |day|
+      lows.push(day[2])
+    end
+    lows.sort[0]
+  end
+
+  def highest_temperature
+    highs = []
+    @data.each do |day|
+      highs.push(day[1])
+    end
+    highs.sort[-1]
+  end
+
+  def day_of_lowest_temperature
+
+    lowest_day = @data.select {|day_num| day_num[2] == lowest_temperature}
+    lowest_day[0][0]
+
+  end
+
+  def day_of_highest_temperature
+
+    highest_day = @data.select {|day_num| day_num[1] == highest_temperature}
+    highest_day[0][0]
+  end
+
+  def average_temperatures
+    @data.map do |day|
+      (((day[1]).to_f + (day[2]).to_f)/2)
+    end
+  end
 end
